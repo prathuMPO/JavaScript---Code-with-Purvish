a2f72d0de8ef17f8ed9d5c862513b0e4dce9d2eb
prathuMPO <122918003+prathuMPO@users.noreply.github.com>
2023-09-25 14:25:16 +0000

    Datatypes done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 1445556
+let accountEmail = "purcishMpt@gmail.com"
+var accountPassword = "25786"
+accoutnCity = "Surat"
+
+// accountId = 2   // not allowed 
+
+accountEmail = "adcuhfnn@yahho.com"
+accountPassword = "24155"
+accoutnCity = "beanglore"
+console.log(accountId);
+let accountState;
+
+/*
+prefer not to use  var,
+beacause of issues of block scope and functional scope
+*/
+
+console.table([accountId, accountEmail,accountPassword,accoutnCity, accountState])
+
+ 
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,48 @@
+"Use Strict"; // treat all the js code newer version
+
+// alert( 3 + 3 ) // We are usin g nodejs not browser
+
+// Code readability is high priority.
+
+// for official documentation of Javascript: tc39.es
+// second for refrence: MDN DOCS
+
+
+let name = "Purvish"
+let age = 21
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigint
+//String => "" prefer double quote
+//boolean => true/false
+//null  = standalone value is one object
+
+//undefined => value is assinged
+
+//sybmol => unique
+
+
+// object 
+
+console.log(typeof undefined) //undefined 
+console.log(typeof null)// is object
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
