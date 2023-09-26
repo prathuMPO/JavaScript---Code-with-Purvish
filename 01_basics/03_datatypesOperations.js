c78ed6705d5ecea3f8b6415b2fb24fabc80509c7
prathuMPO <122918003+prathuMPO@users.noreply.github.com>
2023-09-26 16:10:00 +0000

    datatyes and operations done.

--- 01_basics/03_datatypesOperations.js
+++ 01_basics/03_datatypesOperations.js
@@ -1,4 +1,4 @@
-let score = "true"
+/* let score = "true"
 
 console.log(typeof score);
 console.log(typeof (score));
@@ -24,5 +24,36 @@ let someNumber = 33
 
 let stringnumber = String(someNumber)
 console.log(someNumber);
-console.log(typeof stringnumber);
+console.log(typeof stringnumber); */
 
+// ************** Operations ***********
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+
+let str1 = "hello"
+let str2 = " Purvish"
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "3");
+
+// console.log(+true);
+
+let num1, num2,num3
+
+num1 = num2= num3 = 2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
