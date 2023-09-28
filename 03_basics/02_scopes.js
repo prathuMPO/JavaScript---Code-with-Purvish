b894098bdb606970fd4157c738523ad28883c075
prathuMPO <122918003+prathuMPO@users.noreply.github.com>
2023-09-28 21:46:08 +0000

    Scope done

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,20 @@
+var c = 300
+
+let a= 300  // global scope
+if(true){    // block scope
+    let a = 10
+const b= 25
+var c = 30
+console.log("inner:", a);
+
+}
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    
+}
+
+
+console.log(a)
+// console.log(b)
+// console.log(c)
