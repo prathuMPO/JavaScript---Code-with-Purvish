b0e08a9c2fb10d6201c5787dbde6311e55aee5fc
prathuMPO <122918003+prathuMPO@users.noreply.github.com>
2023-09-28 22:18:11 +0000

    Working on scopes.

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -9,12 +9,47 @@ console.log("inner:", a);
 
 }
 
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
     
-}
+// }
 
 
-console.log(a)
+// console.log(a)
 // console.log(b)
 // console.log(c)
+
+function one(){
+    const username = "poukl"
+
+    function two(){
+        const website = "reddit"
+        console.log(username);
+        console.log(website);
+
+    }
+
+    two()
+}
+
+one()
+
+if(true){
+    const username = "ploip"
+    if(username === "ploip"){
+        const website = " youtube"
+        console.log(username + website)
+    }
+}
+
+//*********** interestinf ********** */
+
+function addOne(num){
+    return num + 1
+}
+// console.log(addOne(5))
+
+const addTwo = function(num){
+    return num + 2
+}
+addTwo(5)
